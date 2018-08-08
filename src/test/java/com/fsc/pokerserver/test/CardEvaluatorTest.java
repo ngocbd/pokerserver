f4610395e207b8441a241e9b77d8d087abfc691c
Ha Bogay <habogay@gmail.com>
2018-08-08 11:22:25 +0700

    Create doc for all files.

--- src/test/java/com/fsc/pokerserver/test/CardEvaluatorTest.java
+++ src/test/java/com/fsc/pokerserver/test/CardEvaluatorTest.java
@@ -1,6 +1,6 @@
 /*
 The MIT License (MIT)
-Copyright (c) 2013 Jacob Kanipe-Illig
+Copyright (c) 2018 by habogay
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -17,6 +17,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
 package com.fsc.pokerserver.test;
 
 import junit.framework.TestCase;
