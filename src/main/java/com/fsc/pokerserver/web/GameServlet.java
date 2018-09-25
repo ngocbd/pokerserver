a4048ce2f4c43868d0265619f1978400cd793233
TonyTruong <truonganhtuan016@gmail.com>
2018-09-25 11:42:40 +0700

    Refactor code in GameServlet + Add API check round is ready or not

--- src/main/java/com/fsc/pokerserver/web/GameServlet.java
+++ src/main/java/com/fsc/pokerserver/web/GameServlet.java
@@ -39,198 +39,145 @@ import com.google.common.base.Joiner;
 
 /**
  * The class to react of game server.
+ *
  * @category com > fcs > pokerserver > web
- * */
-@WebServlet(name = "GameServlet", urlPatterns = { "/api/game" })
+ */
+@WebServlet(name = "GameServlet", urlPatterns = {"/api/game"})
 public class GameServlet extends HttpServlet {
-	
 
-	public MqttServletGameServer server = MqttServletGameServer.getInstance();
-	private static Logger logger = Logger.getLogger(GameServlet.class.getName());
-	
-	@Override 
-	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
-	{ 
-		// TODO Auto-generated method stub.
-		resp.setHeader("Access-Control-Allow-Origin", "*"); 
-		resp.setHeader("Access-Control-Allow-Methods", "GET, POST"); 
-		resp.setHeader("Access-Control-Allow-Headers", "Content-Type, authorization"); 
-		resp.setHeader("Access-Control-Max-Age", "86400"); 
-		resp.setHeader("Cache-Control", "public, max-age=90000"); 
-		// Tell the browser what requests we allow.
-		resp.setHeader("Allow", "GET, HEAD, POST, PUT, TRACE, OPTIONS"); 
-	}
-	
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException
-	{
-		doOptions(request,response);
-		String method = request.getParameter("method");
-		if("put".equalsIgnoreCase(method))
-		{
-			doPut(request,response);
-			return;
-		}//The player join the room
-		else if("join".equalsIgnoreCase(method))
-		{
-			doPost(request,response);
-			return;
-		}//The player start the game
-		else if("start".equalsIgnoreCase(method))
-		{
-			
-			Player p = (Player) request.getAttribute("player");
-			p.getCurrentGame().setDealer(p);
-		
-			int sizeOfListPlayer = p.getCurrentGame().getListPlayer().size();
-			for(int i=0;i<sizeOfListPlayer;i++)
-			{
-				p.getCurrentGame().getListPlayer().get(i).setBalance(1000);
-			}
-			
-			p.getCurrentGame().startGame();
-			
-			logger.log(Level.INFO, "Start Game\n\tDealer: "+ p.getCurrentGame().getDealer().getName()+"\n\tSmall Blind: " + p.getCurrentGame().getSmallBlind().getName()+"\n\tBig Blind: " + p.getCurrentGame().getBigBlind().getName());
 
-			return;
-		}//The preflop of the game
-		else if("preflop".equalsIgnoreCase(method))
-		{
-			
-			Player p = (Player) request.getAttribute("player");
-			
-			p.getCurrentGame().preflop();
-			
-			logger.log(Level.INFO, "The Preflop of the game\n\tNumber of the cards: "+p.getCurrentGame().getBoard().getCardNumber()+"\n\tSmallBlind's balance: " + p.getCurrentGame().getSmallBlind().getBalance()+"\n\tBigBlind's balance: " + p.getCurrentGame().getBigBlind().getBalance()+"\n\tCurrent Player: " + p.getCurrentGame().getCurrentPlayer().getName());
-			
-			return;
-		}//The player bet
-		else if("bet".equalsIgnoreCase(method))
-		{
-			Player p = (Player) request.getAttribute("player");
-			
-			long betValue = Long.parseLong(request.getParameter("value"));
-			
-			p.bet(betValue);
-			
-			logger.log(Level.INFO, "The Player's name : "+p.getName()+"\n\tBet value: "+betValue+"\n\tBalance of Current Player: "+p.getBalance());
-			return;
-		}//The player fold
-		else if("fold".equalsIgnoreCase(method))
-		{
-			Player p = (Player) request.getAttribute("player");
-			
-			p.fold();
-			logger.log(Level.INFO, "The Player folded: "+p.getName());
-			
-			return;
-		}//The flop of the game
-		else if("flop".equalsIgnoreCase(method))
-		{
-			
-			Player p = (Player) request.getAttribute("player");
+    public MqttServletGameServer server = MqttServletGameServer.getInstance();
+    private static Logger logger = Logger.getLogger(GameServlet.class.getName());
 
-			p.getCurrentGame().flop();
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub.
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET, POST");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type, authorization");
+        resp.setHeader("Access-Control-Max-Age", "86400");
+        resp.setHeader("Cache-Control", "public, max-age=90000");
+        // Tell the browser what requests we allow.
+        resp.setHeader("Allow", "GET, HEAD, POST, PUT, TRACE, OPTIONS");
+    }
 
-			String strFlop = "The Flop of the game \n\tNumber of cards: "+ p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: "+ p.getCurrentGame().getBoard().getFlopCards().toString();
-			for(int i=0;i<p.getCurrentGame().getListPlayer().size();i++)
-			{
-				strFlop+="\n\tCards of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString()+" "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
-				strFlop+="\n\tBalance of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getBalance();
-			}
-			logger.log(Level.INFO, strFlop);
-			
-			return;
-		}//The turn of the game
-		else if("turn".equalsIgnoreCase(method))
-		{
-			
-			Player p = (Player) request.getAttribute("player");
-			
-			p.getCurrentGame().turn();
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        doOptions(request, response);
+        String method = request.getParameter("method");
+        Player p = null;
+        switch (method) {
+            case "put":
+                doPut(request, response);
+                break;
+            case "join":
+                doPost(request, response);
+                break;
+            case "start":
+                p = (Player) request.getAttribute("player");
+                p.getCurrentGame().setDealer(p);
+                int sizeOfListPlayer = p.getCurrentGame().getListPlayer().size();
+                for (int i = 0; i < sizeOfListPlayer; i++) {
+                    p.getCurrentGame().getListPlayer().get(i).setBalance(1000);
+                }
+                p.getCurrentGame().startGame();
+                logger.log(Level.INFO, "Start Game\n\tDealer: " + p.getCurrentGame().getDealer().getName() + "\n\tSmall Blind: " + p.getCurrentGame().getSmallBlind().getName() + "\n\tBig Blind: " + p.getCurrentGame().getBigBlind().getName());
+                break;
+            case "preflop":
+                p = (Player) request.getAttribute("player");
+                p.getCurrentGame().preflop();
+                logger.log(Level.INFO, "The Preflop of the game\n\tNumber of the cards: " + p.getCurrentGame().getBoard().getCardNumber() + "\n\tSmallBlind's balance: " + p.getCurrentGame().getSmallBlind().getBalance() + "\n\tBigBlind's balance: " + p.getCurrentGame().getBigBlind().getBalance() + "\n\tCurrent Player: " + p.getCurrentGame().getCurrentPlayer().getName());
+                break;
+            case "bet":
+                p = (Player) request.getAttribute("player");
+                long betValue = Long.parseLong(request.getParameter("value"));
+                p.bet(betValue);
+                logger.log(Level.INFO, "The Player's name : " + p.getName() + "\n\tBet value: " + betValue + "\n\tBalance of Current Player: " + p.getBalance());
+                break;
+            case "fold":
+                p = (Player) request.getAttribute("player");
+                p.fold();
+                logger.log(Level.INFO, "The Player folded: " + p.getName());
+                break;
+            case "flop":
+                p = (Player) request.getAttribute("player");
+                p.getCurrentGame().flop();
+                String strFlop = "The Flop of the game \n\tNumber of cards: " + p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: " + p.getCurrentGame().getBoard().getFlopCards().toString();
+                for (int i = 0; i < p.getCurrentGame().getListPlayer().size(); i++) {
+                    strFlop += "\n\tCards of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString() + " " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
+                    strFlop += "\n\tBalance of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getBalance();
+                }
+                logger.log(Level.INFO, strFlop);
+                break;
+            case "turn":
+                p = (Player) request.getAttribute("player");
+                p.getCurrentGame().turn();
+                String strTurn = "The Turn of the game\n\tNumber of cards: " + p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: " + p.getCurrentGame().getBoard().getFlopCards().toString() + "\n\tThe Cards in the Turn: " + p.getCurrentGame().getBoard().getTurnCard().toString();
+                for (int i = 0; i < p.getCurrentGame().getListPlayer().size(); i++) {
+                    strTurn += "\n\tCards of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString() + " " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
+                    strTurn += "\n\tBalance of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getBalance();
+                }
+                logger.log(Level.INFO, strTurn);
+                break;
+            case "river":
+                p = (Player) request.getAttribute("player");
+                p.getCurrentGame().river();
+                String strRiver = "The River of the game\n\tNumber of cards: " + p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: " + p.getCurrentGame().getBoard().getFlopCards().toString() + "\n\tThe Cards in the Turn: " + p.getCurrentGame().getBoard().getTurnCard().toString() + "\n\tThe Cards in the River: " + p.getCurrentGame().getBoard().getRiverCard().toString();
+                for (int i = 0; i < p.getCurrentGame().getListPlayer().size(); i++) {
+                    strRiver += "\n\tCards of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString() + " " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
+                    strRiver += "\n\tBalance of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getBalance();
+                }
+                logger.log(Level.INFO, strRiver);
+                break;
+            case "end":
+                p = (Player) request.getAttribute("player");
+                p.getCurrentGame().endGame();
+                String strEnd = "The End of the game\n\tNumber of cards: " + p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: " + p.getCurrentGame().getBoard().getFlopCards().toString() + "\n\tThe Cards in the Turn: " + p.getCurrentGame().getBoard().getTurnCard().toString() + "\n\tThe Cards in the River: " + p.getCurrentGame().getBoard().getRiverCard().toString();
+                for (int i = 0; i < p.getCurrentGame().getListPlayer().size(); i++) {
+                    strEnd += "\n\tCards of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString() + " " + p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
+                    strEnd += "\n\tBalance of Player " + (i + 1) + ": " + p.getCurrentGame().getListPlayer().get(i).getBalance();
+                }
+                strEnd += "\n\tThe winner: " + p.getCurrentGame().getWinner() + "\n\tThe rank: " + p.getCurrentGame().getRank() + "\n\tThe best hand:" + p.getCurrentGame().getBestHand();
+                logger.log(Level.INFO, strEnd);
+                break;
+            case "roundcheck":
+                response.getWriter().println(p.getCurrentGame().isNextRoundReady());
+                break;
+            default:
+                String data = Joiner.on(",").join(this.server.getListRoom());
+                logger.log(Level.INFO, data);
+                return;
+        }
+    }
 
-			String strFlop = "The Turn of the game\n\tNumber of cards: "+ p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: "+ p.getCurrentGame().getBoard().getFlopCards().toString()+"\n\tThe Cards in the Turn: "+p.getCurrentGame().getBoard().getTurnCard().toString();
-			for(int i=0;i<p.getCurrentGame().getListPlayer().size();i++)
-			{
-				strFlop+="\n\tCards of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString()+" "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
-				strFlop+="\n\tBalance of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getBalance();
-			}
-			logger.log(Level.INFO, strFlop);
-			
-			return;
-		}//The river of the game
-		else if("river".equalsIgnoreCase(method))
-		{
-			
-			Player p = (Player) request.getAttribute("player");
-			
-			p.getCurrentGame().river();
-		
-			String strFlop = "The River of the game\n\tNumber of cards: "+ p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: "+ p.getCurrentGame().getBoard().getFlopCards().toString()+"\n\tThe Cards in the Turn: "+p.getCurrentGame().getBoard().getTurnCard().toString()+"\n\tThe Cards in the River: "+p.getCurrentGame().getBoard().getRiverCard().toString();
-			for(int i=0;i<p.getCurrentGame().getListPlayer().size();i++)
-			{
-				strFlop+="\n\tCards of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString()+" "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
-				strFlop+="\n\tBalance of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getBalance();
-			}
-			logger.log(Level.INFO, strFlop);
-			
-			return;
-		}//The end of the game
-		else if("end".equalsIgnoreCase(method))
-		{
-			
-			Player p = (Player) request.getAttribute("player");
-			
-			p.getCurrentGame().endGame();
-			
-			
-		
-			String strFlop = "The End of the game\n\tNumber of cards: "+ p.getCurrentGame().getBoard().getCardNumber() + "\n\tThe Cards in the flop: "+ p.getCurrentGame().getBoard().getFlopCards().toString()+"\n\tThe Cards in the Turn: "+p.getCurrentGame().getBoard().getTurnCard().toString()+"\n\tThe Cards in the River: "+p.getCurrentGame().getBoard().getRiverCard().toString();
-			for(int i=0;i<p.getCurrentGame().getListPlayer().size();i++)
-			{
-				strFlop+="\n\tCards of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(0).toString()+" "+p.getCurrentGame().getListPlayer().get(i).getPlayerHand().getCard(1).toString();
-				strFlop+="\n\tBalance of Player "+(i+1)+": "+p.getCurrentGame().getListPlayer().get(i).getBalance();
-			}
-			strFlop += "\n\tThe winner: "+ p.getCurrentGame().getWinner()+"\n\tThe rank: "+p.getCurrentGame().getRank()+"\n\tThe best hand:" + p.getCurrentGame().getBestHand();
-			logger.log(Level.INFO, strFlop);
-			
-			return;
-		}
-		else
-		{
-				String data = Joiner.on(",").join(this.server.getListRoom());
-				logger.log(Level.INFO, data);
-		}
-	}
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
-	{
-		Player p = (Player) request.getAttribute("player");
-		
-		String id = request.getParameter("id");
-		
-		Room room = server.getRoomByID(Long.parseLong(id));
-		
-		checkNotNull(room,"Room "+id+" not found");
-		
-		room.addPlayer(p);
-		
-		String data = Joiner.on(",").join(this.server.getListPlayer());
-		
-		response.getWriter().println(data);
-		
-	}
-	
-	@Override
-	public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		Player p = (Player) request.getAttribute("player");
-		
-		Room room = p.getCurrentRoom();
-		
-		checkNotNull(room,"Room not found");
-		
-		room.createNewGame();
-		
-		response.getWriter().println(room.getCurrentGame().getId()); 
-	}
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Player p = (Player) request.getAttribute("player");
+
+        String id = request.getParameter("id");
+
+        Room room = server.getRoomByID(Long.parseLong(id));
+
+        checkNotNull(room, "Room " + id + " not found");
+
+        room.addPlayer(p);
+
+        String data = Joiner.on(",").join(this.server.getListPlayer());
+
+        response.getWriter().println(data);
+
+    }
+
+    @Override
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Player p = (Player) request.getAttribute("player");
+
+        Room room = p.getCurrentRoom();
+
+        checkNotNull(room, "Room not found");
+
+        room.createNewGame();
+
+        response.getWriter().println(room.getCurrentGame().getId());
+    }
 }
