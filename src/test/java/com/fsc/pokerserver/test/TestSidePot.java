f5208f3dc0200609bf688f2755704d5ed2fecb74
Toản Quốc <toantqd00424@fpt.edu.vn>
2018-10-19 14:04:02 +0700

    update test sidepot

--- src/test/java/com/fsc/pokerserver/test/TestSidePot.java
+++ src/test/java/com/fsc/pokerserver/test/TestSidePot.java
@@ -68,7 +68,7 @@ public class TestSidePot {
         player3.check();
         player4.check();
         player5.check();
-        System.out.println("Pot: "+game.getPotBalance());
+        System.out.println("Pot: " + game.getPotBalance());
 
         /**
          * RIVER now*/
@@ -92,7 +92,7 @@ public class TestSidePot {
         player3.setPlayerHand(new Hand(Card.QUEEN_OF_SPADES, Card.KING_OF_DIAMONDS));
         player4.setPlayerHand(new Hand(Card.QUEEN_OF_DIAMONDS, Card.JACK_OF_HEARTS));
         player5.setPlayerHand(new Hand(Card.QUEEN_OF_CLUBS, Card.TEN_OF_DIAMONDS));
-        System.out.println("Pot: "+game.getPotBalance());
+        System.out.println("Pot: " + game.getPotBalance());
         player5.allIn();
         System.out.println("winner: " + game.getWinners());
         /**
@@ -105,6 +105,98 @@ public class TestSidePot {
         Assert.assertEquals(2000, player4.getBalance());
         //Finally player 5 get his betting of 1000 back due to nobody can match his bet.
         Assert.assertEquals(1000, player5.getBalance());
-        Assert.assertEquals(master.getBalance()+player2.getBalance()+player3.getBalance()+player4.getBalance()+player5.getBalance(),game.getPotBalance());
+        Assert.assertEquals(master.getBalance() + player2.getBalance() + player3.getBalance() + player4.getBalance() + player5.getBalance(), game.getPotBalance());
+    }
+
+    @Test
+    public void sidePot_1Winner2() {
+        Player master = new Player("Master-P2");
+        master.setGlobalBalance(5000);
+        Room room = new Room(master, BlindLevel.BLIND_10_20);
+        Game game = room.createNewGame();
+
+        Player player2 = new Player("Player 2-2");
+        player2.setGlobalBalance(5000);
+        room.addPlayer(player2);
+        Player player3 = new Player("Player 2-3");
+        player3.setGlobalBalance(5000);
+        room.addPlayer(player3);
+        Player player4 = new Player("Player 2-4");
+        player4.setGlobalBalance(5000);
+        room.addPlayer(player4);
+        Player player5 = new Player("Player 2-5");
+        player5.setGlobalBalance(5000);
+        room.addPlayer(player5);
+
+        game.setDealer(player5);
+
+        master.setBalance(1000);
+        player2.setBalance(1050);
+        player3.setBalance(900);
+        player4.setBalance(950);
+        player5.setBalance(1200);
+
+        game.startGame();
+
+        player3.bet(20);
+        player4.bet(20);
+        player5.bet(20);
+        master.bet(10);
+        player2.check();
+
+        Assert.assertEquals(GameStatus.FLOP, game.getStatus());
+        master.bet(20);
+        player2.bet(20);
+        player3.bet(20);
+        player4.bet(20);
+        player5.bet(20);
+
+        Assert.assertEquals(GameStatus.TURN, game.getStatus());
+        master.allIn();
+        player2.bet(960);
+        player3.allIn();
+        player4.allIn();
+        player5.allIn();
+        System.out.println("2-1 : " + player2.getGameBet());
+
+//        master.check();
+        player2.allIn();
+        System.out.println("1 : " + master.getGameBet());
+        System.out.println("2 : " + player2.getGameBet());
+        System.out.println("3 : " + player3.getGameBet());
+        System.out.println("4 : " + player4.getGameBet());
+        System.out.println("5 : " + player5.getGameBet());
+
+        System.out.println("status : " + game.getStatus());
+
+//        Assert.assertEquals(GameStatus.RIVER, game.getStatus());
+        master.check();
+        player2.check();
+        player3.check();
+        player4.check();
+        player5.check();
+
+
+        //SET BOARD AND PLAYER HAND IN ORDER TO IDENTIFY WINNERS.
+        game.setBoard(new Board(Card.TWO_OF_SPADES
+                , Card.THREE_OF_CLUBS
+                , Card.FOUR_OF_SPADES
+                , Card.FIVE_OF_DIAMONDS
+                , Card.SEVEN_OF_CLUBS));
+        //Master win mainpot.
+        master.setPlayerHand(new Hand(Card.FOUR_OF_DIAMONDS, Card.FIVE_OF_HEARTS));
+        //player2 win second pot (1st sidepot)
+        player2.setPlayerHand(new Hand(Card.SEVEN_OF_SPADES, Card.EIGHT_OF_DIAMONDS));
+        //player3 win third pot (2nd sidepot)
+        player3.setPlayerHand(new Hand(Card.QUEEN_OF_SPADES, Card.KING_OF_DIAMONDS));
+        player4.setPlayerHand(new Hand(Card.QUEEN_OF_DIAMONDS, Card.JACK_OF_HEARTS));
+        player5.setPlayerHand(new Hand(Card.QUEEN_OF_CLUBS, Card.TEN_OF_DIAMONDS));
+
+        System.out.println("Pot: " + game.getPotBalance());
+        System.out.println("winner: " + game.getWinners());
+
+//        Assert.assertEquals(GameStatus.END_HAND, game.getStatus());
+
+
     }
 }
